adae7e3d79480b4d3f827ecd465a3388a9b0671a
Gülch János <janso.gulch@gmail.com>
2023-02-18 15:43:21 +0100

    creating and add catchAsync() function to async functions to handle errors

--- app.js
+++ app.js
@@ -3,10 +3,11 @@ const app = express();
 const path = require("path");
 const mongoose = require('mongoose');
 const ejsMate = require("ejs-mate");
+const catchAsync = require("./utils/catchAsync");
 const methodOverride = require("method-override");
 const Campground = require('./models/campground');
 
-mongoose.set('strictQuery',true);
+mongoose.set('strictQuery', true);
 
 mongoose.connect('mongodb://127.0.0.1:27017/yelp-camp');
 
@@ -16,57 +17,67 @@ db.once("open", () => {
     console.log("Database connected");
 });
 
-app.engine('ejs',ejsMate);
+app.engine('ejs', ejsMate);
 app.set("view engine", "ejs");
 app.set("views", path.join(__dirname, "views"));
 
-app.use(express.urlencoded({extended: true})); // hogy parsolja a body-t (req.body) miután submitoltuk a new formot, mert enélkül a body 'üres'
+app.use(express.urlencoded({ extended: true })); // hogy parsolja a body-t (req.body) miután submitoltuk a new formot, mert enélkül a body 'üres'
 app.use(methodOverride("_method"));
 
 app.get('/', (req, res) => {
     res.render("home");
 })
 
-app.get('/campgrounds', async (req, res) => {
+app.get('/campgrounds', catchAsync(async (req, res) => {
     const campgrounds = await Campground.find({});
     res.render('campgrounds/index', { campgrounds });
-});
+}));
 
-app.get('/campgrounds/new', (req, res) =>{
+app.get('/campgrounds/new', (req, res) => {
     res.render('campgrounds/new');
 });
 
-app.get('/campgrounds/:id', async (req, res) =>{
+app.get('/campgrounds/:id', catchAsync(async (req, res) => {
     const campground = await Campground.findById(req.params.id);
     res.render('campgrounds/show', { campground })
-});
+}));
 
-app.post('/campgrounds', async (req, res) =>{
+app.post('/campgrounds', catchAsync( async (req, res, next) => {
     const campground = new Campground(req.body.campground);
     await campground.save();
     res.redirect(`campgrounds/${campground._id}`);
-});
+}));
 
-app.get('/campgrounds/:id/edit', async (req, res) => {
+app.get('/campgrounds/:id/edit', catchAsync(async (req, res) => {
     const campground = await Campground.findById(req.params.id)
     res.render('campgrounds/edit', { campground });
-})
+}));
 
-app.put('/campgrounds/:id', async (req, res) => {
+app.put('/campgrounds/:id', catchAsync(async (req, res) => {
     const { id } = req.params;
     const campground = await Campground.findByIdAndUpdate(id, { ...req.body.campground });
     res.redirect(`/campgrounds/${campground._id}`)
-});
+}));
 
-app.delete('/campgrounds/:id', async (req, res) =>{
+app.delete('/campgrounds/:id', catchAsync(async (req, res) => {
     const { id } = req.params;
     await Campground.findByIdAndDelete(id);
     res.redirect(`/campgrounds/`);
-})
+}));
 
+app.use((err, req, res, next) => {
+    res.send("Oh boy, something went wrong!");
+});
 
 app.listen(3000, () => {
     console.log("serving on port 3000");
 });
+
+
+
+
+
+
+
 
 //https://source.unsplash.com/collection/190727/1600x900
