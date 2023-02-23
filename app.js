7d0721efbe4a0cbc0da233b4bbfe659aed5ff0d4
Gülch János <janso.gulch@gmail.com>
2023-02-23 18:07:25 +0100

    add success and error flash messages to the pages

--- app.js
+++ app.js
@@ -3,6 +3,8 @@ const app = express();
 const path = require("path");
 const mongoose = require('mongoose');
 const ejsMate = require("ejs-mate");
+const session = require("express-session");
+const flash = require("connect-flash");
 const ExpressError = require("./utils/ExpressError");
 const methodOverride = require("method-override");
 
@@ -27,6 +29,24 @@ app.use(express.urlencoded({ extended: true })); // hogy parsolja a body-t (req.
 app.use(methodOverride("_method"));
 app.use(express.static(path.join(__dirname, "public")));
 
+const sessionConfig = {
+    secret: 'thisshouldbeabettersecret!',
+    resave: false,
+    saveUninitialized: true,
+    cookie: {
+        httpOnly: true,
+        expires: Date.now() + 1000 * 60 * 60 * 24 * 7,
+        maxAge: 1000 * 60 * 60 * 24 * 7
+    }
+}
+app.use(session(sessionConfig));
+app.use(flash());
+
+app.use((req, res, next) =>{
+    res.locals.success = req.flash('success');
+    res.locals.error = req.flash('error');
+    next();
+})
 
 app.use('/campgrounds', campgrounds);
 app.use('/campgrounds/:id/reviews', reviews)
